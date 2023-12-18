adba4bcf5462770e7df11c1244c953f38bfe0409
Etienne Delclaux <etienne_delclaux@natural-solutions.eu>
2023-12-18 18:56:18 +0100

    fix: add from_addr attribute and use the value declared in the confifg

--- backend/gncitizen/utils/mail_check.py
+++ backend/gncitizen/utils/mail_check.py
@@ -7,11 +7,11 @@ from itsdangerous import URLSafeTimedSerializer
 
 
 def send_user_email(
-    subject: str, to: str, plain_message: str = None, html_message: str = None
+    subject: str, from_addr: str, to: str, plain_message: str = None, html_message: str = None
 ):
     msg = MIMEMultipart("alternative")
     msg["Subject"] = subject
-    msg["From"] = current_app.config["MAIL"]["MAIL_AUTH_LOGIN"]
+    msg["From"] = from_addr
     msg["To"] = to
     plain_msg = MIMEText(
         html_message,
@@ -53,7 +53,7 @@ def send_user_email(
             str(current_app.config["MAIL"]["MAIL_AUTH_PASSWD"]),
         )
         server.sendmail(
-            current_app.config["MAIL"]["MAIL_AUTH_LOGIN"],
+            from_addr,
             to,
             msg.as_string(),
         )
@@ -65,7 +65,6 @@ def send_user_email(
 
 
 def confirm_user_email(newuser, with_confirm_link=True):
-
     token = generate_confirmation_token(newuser.email)
     subject = current_app.config["CONFIRM_EMAIL"]["SUBJECT"]
     to = newuser.email
@@ -82,28 +81,24 @@ def confirm_user_email(newuser, with_confirm_link=True):
     # Record the MIME  text/html.
     template = current_app.config["CONFIRM_EMAIL"]["HTML_TEMPLATE"]
     if not with_confirm_link:
-        template = current_app.config["CONFIRM_EMAIL"][
-            "NO_VALIDATION_HTML_TEMPLATE"
-        ]
+        template = current_app.config["CONFIRM_EMAIL"]["NO_VALIDATION_HTML_TEMPLATE"]
+    from_addr = current_app.config["CONFIRM_EMAIL"]["FROM"]
     try:
         send_user_email(
             subject,
+            from_addr,
             to,
             html_message=template.format(activate_url=activate_url),
         )
 
     except Exception as e:
         current_app.logger.warning("send confirm_email failled. %s", str(e))
-        return {
-            "message": """ send confirm_email failled: "{}".""".format(str(e))
-        }
+        return {"message": """ send confirm_email failled: "{}".""".format(str(e))}
 
 
 def generate_confirmation_token(email):
     serializer = URLSafeTimedSerializer(current_app.config["SECRET_KEY"])
-    return serializer.dumps(
-        email, salt=current_app.config["CONFIRM_MAIL_SALT"]
-    )
+    return serializer.dumps(email, salt=current_app.config["CONFIRM_MAIL_SALT"])
 
 
 def confirm_token(token):
